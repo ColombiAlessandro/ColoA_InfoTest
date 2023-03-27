4277845de2900d8a0da9b5ef94a45e930b789d7f
ColombiAlessandro <94958005+ColombiAlessandro@users.noreply.github.com>
2023-03-27 08:33:34 +0200

    ColoA_Task1
    ColoA_Task1

---
+++ ColoA_InfoTest/ColoA_Candidato.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ColoA_InfoTest
+{
+    abstract class ColoA_Candidato
+    {
+        private int ColoA_matricola;
+        private string ColoA_nome;
+        public string ColoA_Nome
+        {
+            get { return ColoA_nome; }
+            set { ColoA_nome = value; }
+        }
+        public int ColoA_Matricola
+        {
+            get { return ColoA_matricola; }
+            set { ColoA_matricola = value; }
+        }
+        public ColoA_Candidato()
+        {
+            ColoA_Nome = null;
+            ColoA_Matricola = 0;
+        }
+        public ColoA_Candidato(int matricola)
+        {
+            ColoA_Matricola = matricola;
+            ColoA_Nome = null;
+        }
+        public ColoA_Candidato(string nome)
+        {
+            ColoA_Matricola = 0;
+            ColoA_Nome = nome;
+        }
+        public ColoA_Candidato(int matricola, string nome)
+        {
+            ColoA_Nome = nome;
+            ColoA_Matricola = matricola;
+        }
+        public abstract bool isIdoneo();
+        public abstract int punteggio();
+    }
+}
