77a7685d09cf773daf2ddf41422f133a21a45a34
ColombiAlessandro <94958005+ColombiAlessandro@users.noreply.github.com>
2023-03-27 09:22:25 +0200

    ColoA_Task2Corretto
    correzione di task2

---
+++ ColoA_InfoTest/ColoA_Lavoratore.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ColoA_InfoTest
+{
+    public  class ColoA_Lavoratore : ColoA_Candidato
+    {
+        private int ColoA_esperienze;
+        public int ColoA_Esperienze
+        {
+            get { return ColoA_esperienze; }
+            set { if (value >= 0 && value <= 5)
+                {
+                    ColoA_esperienze = value;
+                }
+                }
+        }
+    }
+}
